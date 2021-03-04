243c6ed894314d6bd2f65e6860d4b118fb2abbb9
unknown <jcambro@umich.edu>
2021-03-04 14:11:19 -0500

    add pandas dataframe to store socket data

--- btc_trading_bot.py
+++ btc_trading_bot.py
@@ -9,24 +9,39 @@ Inspiration and direction from:
 
 USING BINANCE AND API
     * pip install python-binance
+
     * Set the api keys to enviornment variables
         ** 'set binance_api=api_key_here'
         ** 'set binance_secret=api_secret_here'
+
+    * Change api for testing
+        ** client.API_URL = 'https://testnet.binance.vision/api'
 '''
 
 import os
 from binance.client import Client
 from binance.websockets import BinanceSocketManager
 from twisted.internet import reactor
+import pandas as pd
+
+
+TRADING_FEE_PERCENT = 0.0750
+
+# intialize dataframe to hold bitcoin price data along with an error flag
+btc_price = {'BTCUSDT': pd.DataFrame(columns=['date', 'price']), 'error':False}
+
+# logic uses a state machine of either 'buy' or 'sell' depending on if bitcoin
+# is owned already. Default to buy (assume no bitcoin ownership)
+BUY_STATE = True
 
 
 def btc_trade_history(msg):
     ''' define how to process incoming WebSocket messages '''
     if msg['e'] != 'error':
         print(msg['c'])
-        btc_price['close'] = msg['c']   # current days closing price
-        btc_price['bid'] = msg['b']     # best bid price
-        btc_price['ask'] = msg['a']     # best ask price
+        # place the incoming price at the end of the dataframe with the current timestamp
+        btc_price['BTCUSDT'].loc[len(btc_price['BTCUSDT'])] = [pd.Timestamp.now(), float(msg['c'])]
+        btc_price['error'] = False
     else:
         btc_price['error'] = True
 
@@ -38,10 +53,6 @@ def main():
 
     # Intiate the client
     client = Client(api_key, api_secret)
-    btc_price = {'error': False}
-
-    # Manually change api endpoint for testing
-    client.API_URL = 'https://testnet.binance.vision/api'
 
     # start a websocket that will automatically call our btc_trade_history
     # method and update our btc_price dictionary with up-to-date information
@@ -50,7 +61,30 @@ def main():
     bsm.start()
 
     # see bitcoin balance
-    print(client.get_asset_balance(asset='BTC'))
+    # print(client.get_asset_balance(asset='BTC'))
+
+    while len(btc_price['BTCUSDT']) == 0:
+        # wait for Websocket to recieve first data point
+        sleep(0.1)
+
+    # Wait for dataframe to populate for 5 min
+    sleep(300)
+
+    # TODO change to a while True loop later. For testing limit the amount
+    # of loops
+    x = 0
+    while x < 10:
+        # Error check the websocket
+        if btc_price['error']:
+            # restart socket
+            bsm.stop_socket(conn_key)
+            bsm.start()
+            btc_price['error'] = False
+        else:
+            # TODO logic for buying or selling
+            x += 1
+
+        sleep(0.1)
 
     # stop & terminate websocket
     bsm.stop_socket(conn_key)
